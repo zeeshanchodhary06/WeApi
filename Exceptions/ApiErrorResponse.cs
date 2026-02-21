53dcfef9b9261c7046dff53a2076812a29bfe144
zeeshan arshad <zeeshanchodhary06@gmail.com>
2026-02-21 16:59:40 +0500

    [zeeshan] api error response change

--- Exceptions/ApiErrorResponse.cs
+++ Exceptions/ApiErrorResponse.cs
@@ -5,6 +5,6 @@
         public int StatusCode { get; set; }
         public string Message { get; set; } = default!;
         public string TraceId { get; set; } = default!;
-        public object? Errors { get; set; } // validation only
+        public object? Errors { get; set; } // validation purpose only 
     }
 }
